c20d147096d681c203f85a253283406d736826cd
Pablo de Albuquerque <pablo.cda@gmail.com>
2024-04-23 12:17:13 -0300

    add sal_publication_ir

--- models/repository_db/links/sal_publication_ir.sql
+++ models/repository_db/links/sal_publication_ir.sql
@@ -1,10 +1,15 @@
 {{ config(materialized='table') }}
 
-{%- set source_model = 'stg_publication_ir' -%}
-{%- set src_pk = ['publication_hk', 'handle_hk', 'doi_hk'] -%}
-{%- set src_fk = ['internal_identifier', 'handle', 'doi'] -%}
-{%- set src_ldts = 'load_datetime' -%}
-{%- set src_source = 'source' -%}
+with base as (
+    select d.doi, h.handle, i.internal_identifier, d.doi_hk, h.handle_hk, i.publication_hk
+    from {{ ref('stg_publication_ir')}} stg
+    left join {{ ref('hub_publication_doi_ir')}} d on stg.doi_hk =  d.doi_hk
+    left join {{ ref('hub_publication_handle_ir')}} h on stg.handle_hk =  h.handle_hk
+    left join {{ ref('hub_publication_id_ir')}} i on stg.publication_hk = i.publication_hk
+),
 
-{{ automate_dv.link(src_pk=src_pk, src_fk=src_fk, src_ldts=src_ldts,
-                    src_source=src_source, source_model=source_model) }}
+final as (
+    select * from base
+)
+
+select * from final

216fa9ab0a7b2ac30ca8975c137ba2000e643638
Pablo de Albuquerque <pablo.cda@gmail.com>
2024-05-15 13:36:12 -0300

    Agrego dimensiones y fix de raw_item_doi para ir

--- models/ir/raw/source/raw_item_doi.sql
+++ models/ir/raw/source/raw_item_doi.sql
@@ -1,12 +1,10 @@
 with base as (
-    SELECT item_id, d.doi
-    FROM {{ source('ir', 'item') }} i
-    INNER JOIN {{ source('ir', 'doi') }} d ON i.item_id = d.resource_id
-    WHERE d.resource_type_id = 2
+    {{ select_metadatavalue('doi', 'sedici', 'identifier', 'other', True)}}
 ),
 
 final as (
     select * from base
+    where doi similar to 'https?://\S+' and doi like '%doi%'
 )
 
 select * from final

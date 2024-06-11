5d9abe0bb4c8f6b315a7c3019418a3503c65438f
Pablo de Albuquerque <pablo.cda@gmail.com>
2024-06-11 13:09:46 -0300

    refactor transformaciones de doi para ir

--- models/ir/01_raw/intermediate/raw_item_doi.sql
+++ models/ir/01_raw/intermediate/raw_item_doi.sql
@@ -2,27 +2,29 @@ WITH base AS (
     {{ select_metadatavalue('doi', 'sedici', 'identifier', 'other', True)}}
 ),
 
-not_url as (
-    SELECT * FROM base
-    where doi like 'doi:%'
+start_with_doi AS (
+    SELECT * 
+    FROM base
+    WHERE doi LIKE 'doi:%'
 ),
 
-not_url_to_url as (
-    SELECT item_id, replace(not_url.doi, 'doi:', 'https://doi.org') as doi, authority 
-    FROM not_url
+norm_start_with_doi AS (
+    SELECT item_id, replace(doi, 'doi:', 'https://doi.org/') AS doi, authority, 'not_url_to_url' 
+    FROM start_with_doi
 ),
 
-is_url as (
-    SELECT * FROM base
-    where doi similar to 'https?://\S+' and doi like '%doi%'
+is_url AS (
+    SELECT item_id, doi, authority, 'is_url'
+    FROM base
+    WHERE doi similar TO 'https?://\S+' AND doi LIKE '%doi%'
 ),
 
-final as (
+final AS (
     SELECT * 
     FROM is_url 
     UNION 
     SELECT * 
-    FROM not_url_to_url
+    FROM norm_start_with_doi
 )
 
 SELECT * FROM final

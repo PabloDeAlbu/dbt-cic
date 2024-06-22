1cc94043b40d8c78f5de53f2ec3aa2b90de5a154
Pablo de Albuquerque <pablo.cda@gmail.com>
2024-06-22 12:00:46 -0300

    Simplifco raw_item_doi

--- models/ir/01_raw/intermediate/raw_item_doi.sql
+++ models/ir/01_raw/intermediate/raw_item_doi.sql
@@ -2,29 +2,18 @@ WITH base AS (
     {{ select_metadatavalue('doi', 'sedici', 'identifier', 'other', True)}}
 ),
 
-start_with_doi AS (
+include_doi AS (
     SELECT * 
     FROM base
-    WHERE doi LIKE 'doi:%'
-),
-
-norm_start_with_doi AS (
-    SELECT item_id, replace(doi, 'doi:', 'https://doi.org/') AS doi, authority, 'not_url_to_url' 
-    FROM start_with_doi
-),
-
-is_url AS (
-    SELECT item_id, doi, authority, 'is_url'
-    FROM base
-    WHERE doi similar TO 'https?://\S+' AND doi LIKE '%doi%'
+    WHERE doi LIKE '%doi%'
 ),
 
 final AS (
-    SELECT * 
-    FROM is_url 
-    UNION 
-    SELECT * 
-    FROM norm_start_with_doi
+    SELECT 
+        item_id,
+        lower('10.' || split_part(doi, '10.',2)) as doi,
+        authority
+    FROM include_doi
 )
 
 SELECT * FROM final

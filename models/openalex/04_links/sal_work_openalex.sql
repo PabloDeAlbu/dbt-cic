cea1d2f2f65b96d2ceb191f077ee4da8272506a1
Pablo de Albuquerque <pablo.cda@gmail.com>
2024-06-24 10:22:29 -0300

    quito comentarios

--- models/openalex/04_links/sal_work_openalex.sql
+++ models/openalex/04_links/sal_work_openalex.sql
@@ -17,10 +17,6 @@ with base as (
     LEFT JOIN {{ref('hub_pmcid_openalex')}} ON hub_pmcid_openalex.pmcid_hk = link_work_pmcid_openalex.pmcid_hk
     LEFT JOIN {{ref('link_work_pmid_openalex')}} ON hub_work_openalex.work_hk = link_work_pmid_openalex.work_hk
     LEFT JOIN {{ref('hub_pmid_openalex')}} ON hub_pmid_openalex.pmid_hk = link_work_pmid_openalex.pmid_hk
-    {# LEFT JOIN {{ref('link_work_language_openalex')}} ON hub_work_openalex.work_hk = link_work_language_openalex.work_hk
-    LEFT JOIN {{ref('hub_language_openalex')}} ON hub_language_openalex.language_hk = link_work_language_openalex.language_hk
-    LEFT JOIN {{ref('link_work_type_openalex')}} ON hub_work_openalex.work_hk = link_work_type_openalex.work_hk
-    LEFT JOIN {{ref('hub_type_openalex')}} ON hub_type_openalex.type_hk = link_work_type_openalex.type_hk #}
 )
 
 SELECT * FROM base

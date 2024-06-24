77d4f274ac833b3429c68d9fc6ae8373eb046315
Pablo de Albuquerque <pablo.cda@gmail.com>
2024-06-24 11:03:10 -0300

    Agrego datos de dim_resourcetype a fact_publication openaire

--- models/openaire/07_facts/fact_publication_openaire.sql
+++ models/openaire/07_facts/fact_publication_openaire.sql
@@ -8,6 +8,10 @@ WITH base AS (
         sal.pmc,
         sal.pmid,
         sal.uniprot,
+        dim_resourcetype.openaire_resourcetype,
+        dim_resourcetype.coar_label,
+        dim_resourcetype.coar_label_es,
+        dim_resourcetype.coar_uri,
         sat.bestaccessright,
         sat.dateofacceptance,
         sat.publisher,
@@ -20,6 +24,8 @@ WITH base AS (
         sal.load_datetime
     FROM {{ref('sal_researchproduct_openaire')}} sal
     INNER JOIN {{ref('sat_researchproduct_openaire')}} sat ON sal.researchproduct_hk = sat.researchproduct_hk
+    INNER JOIN {{ref('link_researchproduct_resourcetype_openaire')}} link_researchproduct_resourcetype ON sal.researchproduct_hk = link_researchproduct_resourcetype.researchproduct_hk
+    INNER JOIN {{ref('dim_resourcetype_openaire')}} dim_resourcetype ON dim_resourcetype.resourcetype_hk = link_researchproduct_resourcetype.resourcetype_hk
 ),
 
 final AS (

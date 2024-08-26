983b0b2e4f01b2917389ce765b625f76fc0c949f
Pablo de Albuquerque <pablo.cda@gmail.com>
2024-08-26 11:10:08 -0300

    Agrego dim_doi

---
+++ models/mart/01_dimensions/dim_doi.sql
@@ -0,0 +1,43 @@
+WITH 
+hub_doi_openalex AS (
+    SELECT 
+        doi_hk, 
+        doi, 
+        TRUE AS in_openalex,
+        FALSE AS in_openaire,
+        FALSE AS in_oai
+    FROM {{ref('hub_doi_openalex')}}
+),
+
+hub_doi_oai AS (
+    SELECT
+        doi_hk,
+        doi,
+        TRUE AS in_oai,
+        FALSE AS in_openalex,
+        FALSE AS in_openaire
+    FROM {{ref('hub_doi_oai')}}
+),
+
+base AS (
+    SELECT
+        COALESCE(openalex.doi, oai.doi) as doi,
+        COALESCE(openalex.in_oai, oai.in_oai) as in_oai,
+        COALESCE(openalex.in_openalex, oai.in_openalex) as in_openalex,
+        COALESCE(openalex.in_openaire, oai.in_openaire) as in_openaire
+    FROM hub_doi_openalex openalex
+    FULL OUTER JOIN hub_doi_oai oai ON openalex.doi = oai.doi
+    {# FULL OUTER JOIN hub_doi_oai oai ON COALESCE(openalex.doi, oai.doi) = oai.doi #}
+),
+
+final AS (
+    SELECT
+    doi,
+    CONCAT('https://doi.org/', doi) as doi_url,
+    in_oai,
+    in_openaire,
+    in_openalex
+    FROM base
+)
+
+SELECT * FROM final

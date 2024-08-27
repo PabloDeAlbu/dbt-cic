f011928f2c499b5ca29909d2fda7586bbb7f1e17
Pablo de Albuquerque <pablo.cda@gmail.com>
2024-08-27 12:12:44 -0300

    Agrego dim_resource_type

--- models/mart/01_dimensions/dim_resource_type.sql
+++ models/mart/01_dimensions/dim_resource_type.sql
@@ -1,12 +1,25 @@
-WITH hub_work_type_openalex AS (
-    SELECT * FROM {{ref('hub_work_type_openalex')}}
+WITH base AS (
+    SELECT
+        
+        hub_resource_type_coar.coar_uri_hk,
+        sat_resource_type_coar.label,
+        sat_resource_type_coar.parent_label_1,
+        sat_resource_type_coar.parent_label_2,
+        sat_resource_type_coar.parent_label_3,
+        sat_resource_type_coar.label_es,
+        hub_work_type_openalex.key_display_name as openalex_label
+    FROM {{ref('hub_resource_type_coar')}}
+    INNER JOIN {{ref('sat_resource_type_coar')}} ON hub_resource_type_coar.coar_uri_hk = sat_resource_type_coar.coar_uri_hk
+    FULL OUTER JOIN {{ref('work_type_openalex2resource_type_coar')}} openalex2coar ON hub_resource_type_coar.coar_uri = openalex2coar.coar_uri
+    -- TODO hacer join por hashkey
+    FULL OUTER JOIN {{ref('hub_work_type_openalex')}} ON hub_work_type_openalex.key_display_name = openalex2coar.type
 ),
 
-base AS (
-    SELECT map_openalex2coar.label_es, map_openalex2coar.type
-    FROM {{ref('map_openalex-types2coar-types')}} map_openalex2coar
-    INNER JOIN hub_work_type_openalex ON hub_work_type_openalex.type = map_openalex2coar.type
-    )
+final AS (
+    SELECT 
+        *
+    FROM base
+    WHERE openalex_label is not null
+)
 
-SELECT * FROM base
-
+SELECT * FROM final

b957c94aab7ddad20e0ddc1089254249599c995a
Pablo de Albuquerque <pablo.cda@gmail.com>
2024-10-01 10:58:50 -0300

    Agrego nuevo caso para normalizar fechar erroneas Contemplo en str_to_date un caso donde venga el año seguido de un guón

--- macros/str_to_date.sql
+++ macros/str_to_date.sql
@@ -1,6 +1,7 @@
 {% macro str_to_date(str_date) %}
 {# {{str_date}} #}
 (CASE 
+    WHEN ({{str_date}} ~ '^\d{4}-$') THEN CONCAT({{str_date}},'01-01')
     WHEN ({{str_date}} ~ '^\d{4}$') THEN CONCAT({{str_date}},'-01-01')
     WHEN ({{str_date}} ~ '^\d{4}-\d{1,2}$') THEN CONCAT({{str_date}},'-01')
     ELSE {{str_date}}

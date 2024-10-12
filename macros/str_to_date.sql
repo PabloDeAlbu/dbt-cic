2cc56690417765e0b90f03d82999a8730ba0524a
Pablo de Albuquerque <pablo.cda@gmail.com>
2024-10-12 13:57:10 -0300

    Actualizo macro str_to_date
        -- Se maneja  el formato con '/', ej: '2018/12/05' o '2018/12'
        -- Se maneja  el formato 'YYYY-' o 'YYYY'
        -- Se maneja  el formato 'YYYY-MM'
        -- Se maneja  el caso de 'YYYY seguido de texto', ej: '1906 TOMO II'
        -- Se Valida formato general de fecha (YYYY-MM-DD)
        -- Se devuelve una "fecha de error" si el formato es incorrecto o tiene valores inválidos

--- macros/str_to_date.sql
+++ macros/str_to_date.sql
@@ -1,10 +1,34 @@
 {% macro str_to_date(str_date) %}
-{# {{str_date}} #}
 (CASE 
-    WHEN ({{str_date}} ~ '^\d{4}-$') THEN CONCAT({{str_date}},'01-01')
-    WHEN ({{str_date}} ~ '^\d{4}$') THEN CONCAT({{str_date}},'-01-01')
-    WHEN ({{str_date}} ~ '^\d{4}-\d{1,2}$') THEN CONCAT({{str_date}},'-01')
-    ELSE {{str_date}}
-END)::date
+    -- Manejar el formato con '/', ej: '2018/12/05' o '2018/12'
+    WHEN ({{str_date}} ~ '^\d{4}/\d{1,2}/\d{1,2}$') 
+        AND (CAST(SUBSTRING(REPLACE({{str_date}}, '/', '-'), 6, 2) AS INTEGER) BETWEEN 1 AND 12)
+        AND (CAST(SUBSTRING(REPLACE({{str_date}}, '/', '-'), 9, 2) AS INTEGER) BETWEEN 1 AND 31)
+    THEN REPLACE({{str_date}}, '/', '-')
+    
+    WHEN ({{str_date}} ~ '^\d{4}/\d{1,2}$') 
+        AND (CAST(SUBSTRING(REPLACE({{str_date}}, '/', '-'), 6, 2) AS INTEGER) BETWEEN 1 AND 12)
+    THEN CONCAT(REPLACE({{str_date}}, '/', '-'), '-01')
 
+    -- Manejar el formato 'YYYY-' o 'YYYY'
+    WHEN ({{str_date}} ~ '^\d{4}-$') THEN CONCAT({{str_date}}, '01-01')
+    WHEN ({{str_date}} ~ '^\d{4}$') THEN CONCAT({{str_date}}, '-01-01')
+
+    -- Manejar el formato 'YYYY-MM'
+    WHEN ({{str_date}} ~ '^\d{4}-\d{1,2}$')
+        AND (CAST(SUBSTRING({{str_date}}, 6, 2) AS INTEGER) BETWEEN 1 AND 12)
+    THEN CONCAT({{str_date}}, '-01')
+
+    -- Manejar el caso de 'YYYY seguido de texto', ej: '1906 TOMO II'
+    WHEN ({{str_date}} ~ '^\d{4}\s.+$') THEN CONCAT(SUBSTRING({{str_date}}, 1, 4), '-01-01')
+
+    -- Validar formato general de fecha (YYYY-MM-DD)
+    WHEN ({{str_date}} ~ '^\d{4}-\d{1,2}-\d{1,2}$') 
+         AND (CAST(SUBSTRING({{str_date}}, 6, 2) AS INTEGER) BETWEEN 1 AND 12) -- Mes válido
+         AND (CAST(SUBSTRING({{str_date}}, 9, 2) AS INTEGER) BETWEEN 1 AND 31) -- Día válido
+    THEN {{str_date}}
+
+    -- Devolver una "fecha de error" si el formato es incorrecto o tiene valores inválidos
+    ELSE '9999-12-31'
+END)::date
 {% endmacro %}

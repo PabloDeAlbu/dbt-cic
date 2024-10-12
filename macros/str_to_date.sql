fc609f0a49f2fa6863e098c8a90cc0cf2b5716df
Pablo de Albuquerque <pablo.cda@gmail.com>
2024-10-12 13:58:00 -0300

    update comment macro str_to date

--- macros/str_to_date.sql
+++ macros/str_to_date.sql
@@ -1,6 +1,6 @@
 {% macro str_to_date(str_date) %}
 (CASE 
-    -- Manejar el formato con '/', ej: '2018/12/05' o '2018/12'
+    -- Se maneja  el formato con '/', ej: '2018/12/05' o '2018/12'
     WHEN ({{str_date}} ~ '^\d{4}/\d{1,2}/\d{1,2}$') 
         AND (CAST(SUBSTRING(REPLACE({{str_date}}, '/', '-'), 6, 2) AS INTEGER) BETWEEN 1 AND 12)
         AND (CAST(SUBSTRING(REPLACE({{str_date}}, '/', '-'), 9, 2) AS INTEGER) BETWEEN 1 AND 31)
@@ -10,25 +10,25 @@
         AND (CAST(SUBSTRING(REPLACE({{str_date}}, '/', '-'), 6, 2) AS INTEGER) BETWEEN 1 AND 12)
     THEN CONCAT(REPLACE({{str_date}}, '/', '-'), '-01')
 
-    -- Manejar el formato 'YYYY-' o 'YYYY'
+    -- Se maneja  el formato 'YYYY-' o 'YYYY'
     WHEN ({{str_date}} ~ '^\d{4}-$') THEN CONCAT({{str_date}}, '01-01')
     WHEN ({{str_date}} ~ '^\d{4}$') THEN CONCAT({{str_date}}, '-01-01')
 
-    -- Manejar el formato 'YYYY-MM'
+    -- Se maneja  el formato 'YYYY-MM'
     WHEN ({{str_date}} ~ '^\d{4}-\d{1,2}$')
         AND (CAST(SUBSTRING({{str_date}}, 6, 2) AS INTEGER) BETWEEN 1 AND 12)
     THEN CONCAT({{str_date}}, '-01')
 
-    -- Manejar el caso de 'YYYY seguido de texto', ej: '1906 TOMO II'
+    -- Se maneja  el caso de 'YYYY seguido de texto', ej: '1906 TOMO II'
     WHEN ({{str_date}} ~ '^\d{4}\s.+$') THEN CONCAT(SUBSTRING({{str_date}}, 1, 4), '-01-01')
 
-    -- Validar formato general de fecha (YYYY-MM-DD)
+    -- Se Valida formato general de fecha (YYYY-MM-DD)
     WHEN ({{str_date}} ~ '^\d{4}-\d{1,2}-\d{1,2}$') 
          AND (CAST(SUBSTRING({{str_date}}, 6, 2) AS INTEGER) BETWEEN 1 AND 12) -- Mes válido
          AND (CAST(SUBSTRING({{str_date}}, 9, 2) AS INTEGER) BETWEEN 1 AND 31) -- Día válido
     THEN {{str_date}}
 
-    -- Devolver una "fecha de error" si el formato es incorrecto o tiene valores inválidos
+    -- Se devuelve una "fecha de error" si el formato es incorrecto o tiene valores inválidos
     ELSE '9999-12-31'
 END)::date
 {% endmacro %}

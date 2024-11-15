ec936e91ab987ef58c66fe873dca7d476f901901
Pablo de Albuquerque <pablo.cda@gmail.com>
2024-11-15 13:13:50 -0300

    Configuro dim doi para que utlice dspacedb y no oai. Comento momentaneamente el uso de openaire que todavía no esta implementado.

--- models/mart/01_dimensions/dim_doi.sql
+++ models/mart/01_dimensions/dim_doi.sql
@@ -1,3 +1,5 @@
+{{ config(materialized='table') }}
+
 WITH 
 hub_doi_openalex AS (
     SELECT 
@@ -5,39 +7,37 @@ hub_doi_openalex AS (
         doi, 
         TRUE AS in_openalex,
         FALSE AS in_openaire,
-        FALSE AS in_oai
+        FALSE AS in_dspacedb
     FROM {{ref('hub_doi_openalex')}}
 ),
 
-hub_doi_oai AS (
+hub_doi_dspacedb AS (
     SELECT
         doi_hk,
         doi,
-        TRUE AS in_oai,
+        TRUE AS in_dspacedb,
         FALSE AS in_openalex,
         FALSE AS in_openaire
-    FROM {{ref('hub_doi_oai')}}
+    FROM {{ref('hub_doi_dspacedb')}}
 ),
 
 base AS (
     SELECT
-        COALESCE(openalex.doi_hk, oai.doi_hk) as doi_hk,
-        COALESCE(openalex.doi, oai.doi) as doi,
-        COALESCE(openalex.in_oai, oai.in_oai) as in_oai,
-        COALESCE(openalex.in_openalex, oai.in_openalex) as in_openalex,
-        COALESCE(openalex.in_openaire, oai.in_openaire) as in_openaire
+        COALESCE(openalex.doi, dspacedb.doi) as doi,
+        COALESCE(dspacedb.in_dspacedb, openalex.in_dspacedb) as in_dspacedb,
+        {# COALESCE(openalex.in_openaire, dspacedb.in_openaire) as in_openaire, #}
+        COALESCE(openalex.in_openalex, dspacedb.in_openalex) as in_openalex
     FROM hub_doi_openalex openalex
-    FULL OUTER JOIN hub_doi_oai oai ON openalex.doi_hk = oai.doi_hk
-    {# FULL OUTER JOIN hub_doi_oai oai ON COALESCE(openalex.doi, oai.doi) = oai.doi #}
+    FULL OUTER JOIN hub_doi_dspacedb dspacedb ON openalex.doi_hk = dspacedb.doi_hk
+    {# FULL OUTER JOIN hub_doi_dspacedb ir ON COALESCE(openalex.doi, ir.doi) = ir.doi #}
 ),
 
 final AS (
     SELECT
-    doi_hk,
     doi,
     CONCAT('https://doi.org/', doi) as doi_url,
-    in_oai,
-    in_openaire,
+    in_dspacedb,
+    {# in_openaire, #}
     in_openalex
     FROM base
 )

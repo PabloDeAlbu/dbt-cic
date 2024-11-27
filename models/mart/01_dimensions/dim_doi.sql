e04fd8375c2872a4018146a7350abe2d14d21a03
Pablo de Albuquerque <pablo.cda@gmail.com>
2024-11-27 12:08:54 -0300

    Actualizo dim_doi con datos de OpenAIRE

--- models/mart/01_dimensions/dim_doi.sql
+++ models/mart/01_dimensions/dim_doi.sql
@@ -21,15 +21,43 @@ hub_doi_dspacedb AS (
     FROM {{ref('hub_doi_dspacedb')}}
 ),
 
+hub_doi_openaire AS (
+    SELECT
+        doi_hk,
+        doi,
+        FALSE AS in_dspacedb,
+        FALSE AS in_openalex,
+        TRUE AS in_openaire
+    FROM {{ref('hub_openaire_doi')}}
+),
+
 base AS (
     SELECT
-        COALESCE(openalex.doi, dspacedb.doi) as doi,
-        COALESCE(dspacedb.in_dspacedb, openalex.in_dspacedb) as in_dspacedb,
-        {# COALESCE(openalex.in_openaire, dspacedb.in_openaire) as in_openaire, #}
-        COALESCE(openalex.in_openalex, dspacedb.in_openalex) as in_openalex
-    FROM hub_doi_openalex openalex
-    FULL OUTER JOIN hub_doi_dspacedb dspacedb ON openalex.doi_hk = dspacedb.doi_hk
-    {# FULL OUTER JOIN hub_doi_dspacedb ir ON COALESCE(openalex.doi, ir.doi) = ir.doi #}
+        dspacedb.doi_hk,
+        dspacedb.doi,
+        dspacedb.in_dspacedb
+    FROM hub_doi_dspacedb dspacedb
+),
+
+add_openalex AS (
+    SELECT
+        COALESCE(base.doi_hk, openalex.doi_hk) as doi_hk,
+        COALESCE(base.doi, openalex.doi) as doi,
+        COALESCE(base.in_dspacedb, False) as in_dspacedb,
+        openalex.in_openalex
+    FROM base base
+    FULL OUTER JOIN hub_doi_openalex openalex ON openalex.doi_hk = base.doi_hk
+),
+
+add_openaire AS (
+    SELECT
+        COALESCE(add_openalex.doi_hk, openaire.doi_hk) as doi_hk,
+        COALESCE(add_openalex.doi, openaire.doi) as doi,
+        add_openalex.in_dspacedb,
+        add_openalex.in_openalex,
+        COALESCE(openaire.in_openaire, False) as in_openaire
+    FROM add_openalex
+    FULL OUTER JOIN hub_doi_openaire openaire ON openaire.doi_hk = add_openalex.doi_hk
 ),
 
 final AS (
@@ -37,9 +65,9 @@ final AS (
     doi,
     CONCAT('https://doi.org/', doi) as doi_url,
     in_dspacedb,
-    {# in_openaire, #}
+    in_openaire,
     in_openalex
-    FROM base
+    FROM add_openaire
 )
 
 SELECT * FROM final

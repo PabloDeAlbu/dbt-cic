f75aafe0218f7fb298d0824cacf4d64709102a48
Pablo de Albuquerque <pablo.cda@gmail.com>
2025-02-11 10:25:30 -0300

    Limito publicaciones en fact publication openaire que solo tenga una referencia a dim pid

--- models/openaire_graph/04_dm/fact_publication_openaire.sql
+++ models/openaire_graph/04_dm/fact_publication_openaire.sql
@@ -1,11 +1,11 @@
 {{ config(
-    materialized='materialized_view',
+    materialized='table',
     indexes=[
       {'columns': ['doi'], 'type': 'hash'},
     ]
 ) }}
 
-WITH base as (
+with base as (
     SELECT
         hub.researchproduct_id as researchproduct_id,
         {# sat.description, #}
@@ -38,7 +38,8 @@ WITH base as (
         dim_type.coar_label_es,
         dim_type.coar_resourcetype_uri,
         dim_accessright.best_access_right,
-        dim_accessright.is_oa
+        dim_accessright.is_oa--,
+--        hub_url.url
     FROM {{ref('hub_openaire_graph_researchproduct')}} hub
     INNER JOIN {{ref('sat_openaire_graph_researchproduct')}} sat_graph ON sat_graph.researchproduct_hk = hub.researchproduct_hk
     INNER JOIN {{ref('dim_pid_openaire_graph')}} dim_pid ON dim_pid.researchproduct_hk = hub.researchproduct_hk
@@ -46,6 +47,13 @@ WITH base as (
     INNER JOIN {{ref('dim_resourcetype_openaire')}} dim_type ON dim_type.type_hk = link.type_hk
     INNER JOIN {{ref('link_openaire_graph_researchproduct2accessright')}} link_accessright ON link_accessright.researchproduct_hk = hub.researchproduct_hk
     INNER JOIN {{ref('dim_accessright_openaire')}} dim_accessright ON dim_accessright.accessright_hk = link_accessright.accessright_hk
+    -- INNER JOIN {{ref('link_openaire_graph_researchproduct2url')}} link_url ON link_url.researchproduct_hk = hub.researchproduct_hk
+    --INNER JOIN {{ref('hub_openaire_graph_url')}} hub_url ON hub_url.url_hk = link_url.url_hk
 )
 
 SELECT * FROM base
+--SELECT url, count(*) FROM base group by url order by count desc
+
+-- SELECT * FROM base where url = 'https://www.ncbi.nlm.nih.gov/pubmed/26623351'
+
+-- SELECT DISTINCT * FROM base where researchproduct_id = 'doi_dedup___::023cd687bf7daf0ecd66c520d3508c0a';
